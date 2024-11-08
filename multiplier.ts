eb96d73a1914f9459bc6e27aa975e048f300dab7
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-11-08 13:33:21 +0200

    call function

--- multiplier.ts
+++ multiplier.ts
@@ -1,3 +1,4 @@
-const multiplier = (a,b,printText)=>{
-    console.log(printText,a*b);
+const multiplicator = (a, b, printText) => {
+    console.log(printText, a * b);
 }
+multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:')
