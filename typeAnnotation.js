88281b274aa04365787d357ce2ac465dbdafda64
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-11-08 11:22:45 +0200

    commit transpiled js file

---
+++ typeAnnotation.js
@@ -0,0 +1,6 @@
+var birthdayGreeter = function (name, age) {
+    return "Happy birthday ".concat(name, ", you are ").concat(age, " years old");
+};
+var birthdayHero = "Mlo Dev";
+var age = 30;
+console.log(birthdayGreeter(birthdayHero, age));
