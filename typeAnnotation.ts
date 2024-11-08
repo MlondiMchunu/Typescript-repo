51bf91d776e5cfd82212f1719268803c9a8c0b1e
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-11-08 11:15:50 +0200

    call function

--- typeAnnotation.ts
+++ typeAnnotation.ts
@@ -1,3 +1,8 @@
 const birthdayGreeter = (name: string, age: number):string=>{
     return `Happy birthday ${name}, you are ${age} years old`
 }
+
+const birthdayHero = "Mlo Dev"
+const age = 30
+
+console.log(birthdayGreeter(birthdayHero,age))
