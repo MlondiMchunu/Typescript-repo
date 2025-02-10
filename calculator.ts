3152726a0324fb7f7c2655218dfa32fc295410aa
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-02-10 17:04:50 +0200

    complete error handling

--- calculator.ts
+++ calculator.ts
@@ -1,7 +1,7 @@
 type Operation = 'multiply' | 'add' | 'divide';
 type result = number | string;
 
-const calculator = (a: number, b: number, op: Operation):number => {
+const calculator = (a: number, b: number, op: Operation): number => {
     /**if (op === 'multiply') {
         return a * b;
     } else if (op === 'add') {
@@ -10,18 +10,24 @@ const calculator = (a: number, b: number, op: Operation):number => {
         if (b === 0) return 'can\'t divide by 0!';
         return a / b;
     }**/
-   switch(op){
-    case 'multiply':
-        return a * b;
-    case 'divide':
-        if(b===0) throw new Error('Can\'t divide by 0!');
-        return a/b;
-    case 'add':
-        return a + b;
+    switch (op) {
+        case 'multiply':
+            return a * b;
+        case 'divide':
+            if (b === 0) throw new Error('Can\'t divide by 0!');
+            return a / b;
+        case 'add':
+            return a + b;
         default:
             throw new Error('Operation is not multiply, add or divide');
-   }
+    }
 }
-
-
-console.log(calculator(7,4,'divide'));
+try {
+    console.log(calculator(7, 4, 'divide'));
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong:'
+    if (error instanceof Error) {
+        errorMessage += error.message;
+    }
+    console.log(errorMessage);
+}
