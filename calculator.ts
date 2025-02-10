032270ddf66021769f6e2139a3b334f410f0d7cb
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-02-10 15:34:22 +0200

    commit switch case statement for error handling

--- calculator.ts
+++ calculator.ts
@@ -2,14 +2,26 @@ type Operation = 'multiply' | 'add' | 'divide';
 type result = number | string;
 
 const calculator = (a: number, b: number, op: Operation):result => {
-    if (op === 'multiply') {
+    /**if (op === 'multiply') {
         return a * b;
     } else if (op === 'add') {
         return a + b;
     } else if (op === 'divide') {
         if (b === 0) return 'can\'t divide by 0!';
         return a / b;
-    }
+    }**/
+   switch(op){
+    case 'multiply':
+        return a * b;
+    case 'divide':
+        if(b===0) throw new Error('Can\'t divide by 0!');
+        return a/b;
+    case 'add':
+        return a + b;
+        default:
+            throw new Error('Operation is not multiply, add or divide');
+   }
 }
+
 
 console.log(calculator(7,4,'divide'));
