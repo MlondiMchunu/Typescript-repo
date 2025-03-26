b57872440635dd63c91f11fc1ba9ebaadb9977eb
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-03-26 23:18:21 +0200

    commit basicCalc

--- basicCalc.ts
+++ basicCalc.ts
@@ -3,7 +3,14 @@ let number = 2;
 let result;
 
 const calculate = (a, b) => {
-if(Number.isInteger(a)===false || Number.isInteger(b)===false){
-    throw new Error("Invalid type: a parameter is not an interger!!");
-}
-}
+    if (Number.isInteger(a) === false || Number.isInteger(b) === false) {
+        throw new Error("Invalid type: a parameter is not an interger!!");
+    } else {
+        return a + b;
+    }
+};
+result = calculate(number, number);
+console.log("value: ", result, " type of ", typeof (result));
+
+result = calculate(number, string);
+console.log("value: ",result," type of",typeof(result));
