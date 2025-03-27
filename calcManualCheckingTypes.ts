3dbf62cbf7238ef4e959b188000a7fcb3a0e1f9d
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-03-27 12:48:54 +0200

    commit  union type

--- calcManualCheckingTypes.ts
+++ calcManualCheckingTypes.ts
@@ -3,6 +3,8 @@ let number = 2;
 let result;
 
 const calculate = (a, b) => {
+
+    //use isInteger function of the number object
     if (Number.isInteger(a) === false || Number.isInteger(b) === false) {
         throw new Error("Invalid type: a parameter is not an interger!!");
     } else {
--- typeAnnotation.ts
+++ typeAnnotation.ts
@@ -1,9 +1,14 @@
-const birthdayGreeter = (name: string, age: number):string=>{
+const birthdayGreeter = (name: string, age: number): string => {
     return `Happy birthday ${name}, you are ${age} years old`
 }
 
 const birthdayHero = "Mlo Dev"
 const age = 30
 
-console.log(birthdayGreeter(birthdayHero,age))
+console.log(birthdayGreeter(birthdayHero, age))
 
+
+let stringOrNumber: string | number;
+stringOrNumber = "mlo";
+stringOrNumber = 30;
+stringOrNumber = true
